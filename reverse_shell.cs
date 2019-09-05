cf38aa2104478322f593da97961bdc36aa0f6ae2
Scrow <37299144+S-crow@users.noreply.github.com>
2019-09-05 16:06:59 +0200

    Update reverse_shell.cs

--- reverse_shell.cs
+++ reverse_shell.cs
@@ -3,7 +3,7 @@ using System.Text;
 using System.IO;
 using System.Diagnostics;
 using System.ComponentModel;
-using System.Data.Linq;
+using System.Linq from System.Core;
 using System.Net;
 using System.Net.Sockets;
 
